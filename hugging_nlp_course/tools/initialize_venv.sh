5793cbec94e30aee45a6c30212e2d0b7924d06e8
Adam Hjerpe <adam.hjerpe@ica.se>
2023-08-21 05:00:28 +0000

    WIP: Add datasets

--- hugging_nlp_course/tools/initialize_venv.sh
+++ hugging_nlp_course/tools/initialize_venv.sh
@@ -4,16 +4,16 @@
 venv_path="/workspaces/exploring/hugging_nlp_course/venvs"
 default_venv="default_venv"
 echo "$venv_path/$default_venv"
-# Check if the virtual environment directory exists
+# Check if the virtual environment directory does not exists
 if [ ! -d "$venv_path/$default_venv" ] ; then
     echo "Virtual environment does not exis"
     echo "Create environment $venv_path/$default_venv"
     python3 -m venv $venv_path/$default_venv --system-site-packages --upgrade-deps
+    echo "Installing requirements.txt"
+    pip install -r requirements.txt
+    # Install ipykernel
 fi
 # Activate and install packages
 echo "Activating environment: $venv_path/$default_venv/bin/activate"
 source "$venv_path/$default_venv/bin/activate"
-echo "Installing requirements.txt"
-pip install -r requirements.txt
-# Install ipykernel
 python -m ipykernel install --user --name=$default_venv
