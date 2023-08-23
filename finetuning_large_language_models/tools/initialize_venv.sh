8575ee61960eec3ed6c372891ef6a2c4ec4afc6a
Adam Hjerpe <adam.hjerpe@ica.se>
2023-08-23 19:39:41 +0000

    WIP: start finetuning llm DL-AI course

---
+++ finetuning_large_language_models/tools/initialize_venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the path to your virtual environment
+#project_name="finetuning_large_language_models"
+SCRIPT_DIR_PARENT=$( cd -- "$( dirname -- "$(dirname -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
+venv_path="$SCRIPT_DIR_PARENT/venvs"
+default_venv="default_venv"
+echo "$venv_path/$default_venv"
+# Check if the virtual environment directory does not exists
+if [ ! -d "$venv_path/$default_venv" ] ; then
+    echo "Virtual environment does not exis"
+    echo "Create environment $venv_path/$default_venv"
+    python3 -m venv $venv_path/$default_venv --system-site-packages --upgrade-deps
+    source "$venv_path/$default_venv/bin/activate"
+    echo "Installing requirements.txt"
+    pip3 install -r requirements.txt
+    pip3 install ipykernel
+    echo "Create ipykernel $default_venv"
+    # Install ipykernel
+    python -m ipykernel install --user --name=$default_venv
+fi
+echo "Activating environment: $venv_path/$default_venv/bin/activate"
+source "$venv_path/$default_venv/bin/activate"
