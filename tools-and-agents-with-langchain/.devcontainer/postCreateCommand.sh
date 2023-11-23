723cca67d535b99ae1079591c9a7aade6d8d4d43
Adam Hjerpe (bbm773) <adam.hjerpe@ica.se>
2023-11-23 06:48:21 +0000

    Tools and agents with langchain

--- tools-and-agents-with-langchain/.devcontainer/postCreateCommand.sh
+++ tools-and-agents-with-langchain/.devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#project_name="PodPal"
+
 SCRIPT_DIR_PARENT=$( cd -- "$( dirname -- "$(dirname -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 echo source $SCRIPT_DIR_PARENT/tools/initialize_venv.sh >> ~/.bashrc
+echo source $SCRIPT_DIR_PARENT/tools/set_envs.sh >> ~/.bashrc
 sudo apt-get --yes update && sudo apt-get --yes upgrade
--- vertex-ai-applying-embeddings/.devcontainer/postCreateCommand.sh
+++ vertex-ai-applying-embeddings/.devcontainer/postCreateCommand.sh
@@ -2,4 +2,4 @@
 sudo apt-get --yes install python3.8-venv
 SCRIPT_DIR_PARENT=$( cd -- "$( dirname -- "$(dirname -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 echo source $SCRIPT_DIR_PARENT/tools/initialize_venv.sh >> ~/.bashrc
-sudo apt-get --yes update && sudo apt-get --yes upgrade
+sudo apt-get --yes update && sudo apt-get --yes upgrade && sudo apt-get install -y python3.8-venv
